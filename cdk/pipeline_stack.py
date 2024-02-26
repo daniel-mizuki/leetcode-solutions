47a77354107ead903e3ebe4f28ab8ac96d61d47d
Daniel Mizuki <dmizuki12@gmail.com>
2024-02-26 14:52:45 -0800

    feat: Add initial code for CDK pipeline

---
+++ cdk/pipeline_stack.py
@@ -0,0 +1,37 @@
+"""
+Pipeline Stack
+"""
+
+from constructs import Construct
+from aws_cdk import (
+    Stack,
+    pipelines as pipelines,
+)
+
+
+class PipelineStack(Stack):
+    """
+    Pipeline Stack
+    """
+
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        source = pipelines.CodePipelineSource.git_hub(
+            "daniel-mizuki/leetcode-solutions",
+            "main",
+        )
+
+        pipeline = pipelines.CodePipeline(
+            self,
+            "Pipeline",
+            synth=pipelines.ShellStep(
+                "Synth",
+                input=source,
+                commands=[
+                    "npm install -g aws-cdk",
+                    "pip install -r requirements.txt",
+                    "cdk synth",
+                ],
+            ),
+        )
