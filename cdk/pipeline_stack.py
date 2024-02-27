9ff4f77d095d459c12b5f5fdf929ed21e82d7c8b
Daniel Mizuki <dmizuki12@gmail.com>
2024-02-26 16:19:34 -0800

    refactor: Rename parameter to address warning

--- cdk/pipeline_stack.py
+++ cdk/pipeline_stack.py
@@ -14,8 +14,8 @@ class PipelineStack(Stack):
     Pipeline Stack
     """
 
-    def __init__(self, scope: Construct, cdk_id: str, **kwargs) -> None:
-        super().__init__(scope, cdk_id, **kwargs)
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
 
         source = pipelines.CodePipelineSource.git_hub(
             "daniel-mizuki/leetcode-solutions",
