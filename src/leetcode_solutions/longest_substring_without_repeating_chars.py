271542b90b1b7718666194e119294eabcd6e6b0e
Daniel Mizuki <dmizuki12@gmail.com>
2024-03-12 00:10:11 -0700

    refactor: Implement minor optimization

--- src/leetcode_solutions/longest_substring_without_repeating_chars.py
+++ src/leetcode_solutions/longest_substring_without_repeating_chars.py
@@ -21,32 +21,32 @@ class Solution:
             int: Length of the longest substring
         """
 
-        # max_len = 0
-        # substr_start = 0
-        # seen = {}
-
-        # for i, char in enumerate(s):
-        #     if char in seen and seen[char] >= substr_start:
-        #         substr_start = seen[char] + 1
-        #     else:
-        #         max_len = max(max_len, i - substr_start + 1)
-
-        #     seen[char] = i
-
-        # return max_len
-
         max_len = 0
         substr_start = 0
-        seen = set()
+        seen = {}
 
         for i, char in enumerate(s):
-            while char in seen:
-                seen.remove(s[substr_start])
-                substr_start += 1
-
-            if (curr_len := i - substr_start + 1) > max_len:
+            if char in seen and seen[char] >= substr_start:
+                substr_start = seen[char] + 1
+            elif (curr_len := i - substr_start + 1) > max_len:
                 max_len = curr_len
 
-            seen.add(char)
+            seen[char] = i
 
         return max_len
+
+        # max_len = 0
+        # substr_start = 0
+        # seen = set()
+
+        # for i, char in enumerate(s):
+        #     while char in seen:
+        #         seen.remove(s[substr_start])
+        #         substr_start += 1
+
+        #     if (curr_len := i - substr_start + 1) > max_len:
+        #         max_len = curr_len
+
+        #     seen.add(char)
+
+        # return max_len
